af0ff7fe101e7a9a4e1ab95a256a8fadff4d0655
Kasun Rajapakse <kasunsjc@hotmail.com>
2016-03-30 16:23:37 +0530

    Inetial-Commit

---
+++ 30-03-2016/AzureRMDeployment.ps1
@@ -0,0 +1,22 @@
+﻿#Connect Azure account with PowerShell
+
+Add-AzureAccount
+
+#Select the subscription (Login to AzureRM Account)
+
+Login-AzureRmAccount 
+
+#create resorce group for deployment 
+
+New-AzureRmResourceGroup -Name "ARM" -Location "Southeast Asia" -Verbose
+
+#Deploy Azure VM Using ARM template using local file system location
+
+$username = "localadmin"
+$password = ConvertTo-SecureString -String 'Pa$$w0rd' -AsPlainText -Force
+$dnsprifix = "kasunvm1"
+
+New-AzureRmResourceGroupDeployment -Name "TestDeploy" -ResourceGroupName "ARM" `
+-TemplateParameterFile '.\101-vm-simple-windows\azuredeploy.parameters.json' `
+-TemplateFile '.\101-vm-simple-windows\azuredeploy.json' `
+-adminUsername $username -adminPassword $password  -dnsLabelPrefix $dnsprifix -Verbose
---
+++ 30-03-2016/MultiVMDeploy.ps1
@@ -0,0 +1,24 @@
+﻿#Add Azure Acount
+
+Add-AzureAccount 
+
+#login to Azure RM Account
+
+Login-AzureRmAccount -TenantId ee625913-4a52-403b-8a2e-e97c111417fb
+
+#create/Get new resource group in the subscription
+
+$RGName =New-AzureRmResourceGroup -Name MultiVM -Location "Southeast Asia" -Verbose
+
+$RGName =Get-AzureRmResourceGroup -Name MultiVM -Location "Southeast Asia" -Verbose
+
+#Set the location of the PS prompt 
+
+Set-Location "E:\SCU Demos\MultiVM"
+
+#Set the Veriables for paremeteras 
+
+$cred = Get-Credential 
+
+New-AzureRmResourceGroupDeployment -Name MultiVM -ResourceGroupName $RGName.ResourceGroupName -TemplateParameterFile .\azuredeploy.parameters.json `
+-TemplateFile .\azuredeploy.json -adminUsername $cred.UserName -adminPassword $cred.Password -vmCount "4" -Verbose
